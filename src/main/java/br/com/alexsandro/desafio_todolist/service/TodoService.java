842460380f2ca1493d5bfd17c55a0197e076050a
Alexdevsoft <alexhavilla2022@gmail.com>
2025-03-28 13:48:10 -0300

    Todo Controller

--- src/main/java/br/com/alexsandro/desafio_todolist/service/TodoService.java
+++ src/main/java/br/com/alexsandro/desafio_todolist/service/TodoService.java
@@ -18,6 +18,7 @@ public class TodoService {
 
     public List<Todo> create(Todo todo) {
         todoRepository.save(todo);
+        return list();
     }
 
     public List<Todo> list() {
@@ -26,12 +27,14 @@ public class TodoService {
         return todoRepository.findAll(sort);
     }
 
-    public List<Todo> update() {
-
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
     }
 
-    public List<Todo> delete() {
-
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
     }
 
 }
